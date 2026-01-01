ca2bbccae342c1b815b7fed4ebf89d34267b2a8f
Ramiz Loki <r.loki@masoft.ch>
2026-01-01 17:46:49 +0100

    chore: finalize storybook 10 migration and fix angular builder error

--- projects/mlpm/.storybook/main.ts
+++ projects/mlpm/.storybook/main.ts
@@ -2,7 +2,12 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-a11y', '@storybook/addon-docs', '@storybook/addon-onboarding'],
-  framework: '@storybook/angular',
+  addons: ['@storybook/addon-a11y', '@storybook/addon-onboarding', '@storybook/addon-docs'],
+  framework: {
+    name: '@storybook/angular',
+    options: {
+      projectName: 'mlpm',
+    },
+  },
 };
 export default config;
