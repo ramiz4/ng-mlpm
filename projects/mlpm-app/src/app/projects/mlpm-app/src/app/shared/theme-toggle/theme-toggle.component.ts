d47fb32a58ecf267f6707bfb46c51b243dd5dfc2
Ramiz Loki <r.loki@masoft.ch>
2026-01-01 16:48:42 +0100

    chore: migrate from demo app to storybook

--- projects/mlpm-app/src/app/projects/mlpm-app/src/app/shared/theme-toggle/theme-toggle.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-theme-toggle',
-  imports: [],
-  templateUrl: './theme-toggle.component.html',
-  styleUrl: './theme-toggle.component.scss',
-})
-export class ThemeToggleComponent {}
